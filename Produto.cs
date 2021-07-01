2b8c019a85ebf35baea70cfe061a199b01a3ea31
Diego Barata <79817829+Diego244@users.noreply.github.com>
2021-07-01 16:31:31 -0300

    Create Produto.cs

---
+++ Produto.cs
@@ -0,0 +1,26 @@
+using System
+using System.Collections.Generic;
+
+namespace DiegoB
+{
+    public abstract class Produto : IImprimivel
+    {
+        
+        protected string _nome;
+        protected double _preco;
+        
+        public double Preco
+        {
+            get { return this._preco;}
+        }
+        
+        public string Nome
+        {
+            get { return this._nome; }
+        }
+        
+        public abstract double CalculaValorTotal();
+        public abstract void Imprimir();
+        
+    }
+}
